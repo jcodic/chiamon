14c1bf7d3d7b9906c840a2a12344d248849f3457
jcodic <68349218+jcodic@users.noreply.github.com>
2021-07-30 23:15:27 +0400

    Add files via upload

--- run_profit_calc.bat
+++ run_profit_calc.bat
@@ -1,2 +1,1 @@
-@rem profit_calc <total_disk_space_tb> <your_disk_space_tb> <coin_price_usd> [rub_per_usd]
-java -cp "target\*" com.ddx.chiamon.client.ProfitCalc 32_000_000 272 180
+java -cp "target\*" com.ddx.chiamon.client.ProfitCalc %*
---
+++ run_profit_calc.sh
@@ -0,0 +1,1 @@
+java -cp "target/*" com.ddx.chiamon.client.ProfitCalc $*
