e92d93d3dcaada263451df891fc7d533c878c5c7
jcodic <68349218+jcodic@users.noreply.github.com>
2021-07-30 20:17:32 +0400

    Add files via upload

---
+++ run_client.sh
@@ -0,0 +1,1 @@
+java -Xms256m -Xmx1g -cp "target/*" -Dlog4j.configurationFile=client_log.xml com.ddx.chiamon.client.ChiaMonClient "ini=client.ini"
---
+++ run_profit_calc.bat
@@ -0,0 +1,2 @@
+@rem profit_calc <total_disk_space_tb> <your_disk_space_tb> <coin_price_usd> [rub_per_usd]
+java -cp "target\*" com.ddx.chiamon.client.ProfitCalc 32_000_000 272 180
